1d7e217bda78544661e6180c96f237d78bb12c52
Mirumo <mysteryperson520@gmail.com>
2022-08-05 18:34:23 +0800

    feat: add root component and use react router

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+function App(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Root Component
+import App from './App';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
