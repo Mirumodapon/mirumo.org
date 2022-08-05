eda7b7626cc15ed9cac65d90d7a366052268d97b
Mirumo <mysteryperson520@gmail.com>
2022-08-05 19:08:51 +0800

    feat: config global css
    Create index.css and import to main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 // Root Component
 import App from './App';
 
+// Global Style
+import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
