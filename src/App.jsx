3b9f5a30ccdc3337b341424e414d17e8c6f51b16
Mirumo <mysteryperson520@gmail.com>
2022-08-07 23:35:08 +0800

    feat(App): add root componet with router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+// Pages
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Root Components
 import App from './App';
+
+// Global css
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
