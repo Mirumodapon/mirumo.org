a4d79f96aeda35bb1fc7f1269225f88d5e019d02
Mirumo <mysteryperson520@gmail.com>
2022-08-08 00:10:44 +0800

    feat: fix path script

--- fix-path.sh
+++ fix-path.sh
@@ -1,4 +1,6 @@
 mkdir ./dist/about && cp ./dist/index.html ./dist/about/index.html
-
+mkdir ./dist/link && cp ./dist/index.html ./dist/link/index.html
+mkdir ./dist/tools && cp ./dist/index.html ./dist/tools/index.html
+mkdir ./dist/sm && cp ./dist/index.html ./dist/sm/index.html
 
 echo mirumo.org > ./dist/CNAME
