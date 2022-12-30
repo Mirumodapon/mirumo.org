e25de0e6b37811db94c6b3f984ade6dd05f39bd4
Mirumo <mysteryperson520@gmail.com>
2022-12-30 19:58:06 +0800

    feat: use redux in root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Store
+import { Provider } from 'react-redux';
+import store from './Store';
+
 // Pages
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -14,19 +18,21 @@ import Clock from './Pages/Clock';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/link" element={<Link />} />
-        <Route path="/tools" element={<Tools />} />
-        <Route path="/sm" element={<ScreenMessage />} />
-        <Route path="/qr" element={<QRcode />} />
-        <Route path="/ran/*" element={<Random />} />
-        <Route path="/pdf-viewer" element={<PdfSide />} />
-        <Route path="/clock" element={<Clock />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/link" element={<Link />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/sm" element={<ScreenMessage />} />
+          <Route path="/qr" element={<QRcode />} />
+          <Route path="/ran/*" element={<Random />} />
+          <Route path="/pdf-viewer" element={<PdfSide />} />
+          <Route path="/clock" element={<Clock />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
