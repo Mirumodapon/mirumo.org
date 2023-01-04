d395656079d79cc5011dcda3372c9dc9765e71f0
Mirumo <mysteryperson520@gmail.com>
2023-01-04 15:02:26 +0800

    refactor: link page

--- src/Pages/Link.jsx
+++ src/Pages/Link.jsx
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeMode } from '../Store/mode.slice';
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
+import axios from 'axios';
 
 const Main = styled.main`
-  width: min(500px, 100vw);
+  width: 100vw;
   height: 100vh;
-  margin: 0 auto;
-  padding: 10px 0;
-  h1 {
-    font-size: 1.3em;
-    text-align: center;
-    &::before,
-    &::after {
-      content: '-';
-      margin: 0 5px;
-    }
+  background-color: ${(props) => props[props.theme].background};
+  color: ${(props) => props[props.theme].content};
+
+  & :is(article, section, h1, a) {
+    margin: 0;
+    padding: 0;
+    margin: 0;
+    border: 0;
   }
+
+  article {
+    width: min(500px, 100vw);
+    padding: 20px 0;
+    margin: 0 auto;
+  }
+
+  section {
+  }
+
+  h1 {
+    text-align: center;
+  }
+
   a {
+    display: block;
+    border: 2px solid ${(props) => props[props.theme].content};
     display: flex;
     width: 80%;
     height: 40px;
     margin: 20px auto;
-    border: 2px solid black;
     border-radius: 20px;
     text-align: center;
     color: inherit;
@@ -28,43 +45,64 @@ const Main = styled.main`
     align-items: center;
     justify-content: center;
     font-weight: bold;
-    &:hover {
-      background: tomato;
-      color: gold;
-      font-weight: bold;
-    }
+  }
+
+  .themeTrigger {
+    display: block;
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    font-size: 24px;
   }
 `;
 
+Main.defaultProps = {
+  dark: {
+    content: 'white',
+    background: '#191919'
+  },
+  light: {
+    content: '#191919',
+    background: 'white'
+  }
+};
+
 function Link() {
-  const [sections, setSections] = useState([]);
-  const now = Date.now() / 1000;
+  const mode = useSelector((s) => s.themeMode);
+  const [lns, setIns] = useState({ sections: [] });
 
-  const linkFilter = (x) =>
-    x.iat >= 0 && x.exp >= 0 && ((x.iat === 0 && x.exp === 0) || (x.iat < now && x.exp > now));
+  const dispatch = useDispatch();
 
-  const fetchLinks = () =>
-    fetch('https://raw.githubusercontent.com/Mirumodapon/mirumo.org/link/link.json')
-      .then((x) => x.json())
-      .then((x) => x.sections)
-      .then((x) => setSections(x));
-
-  useEffect(function () {
-    fetchLinks();
+  useEffect(() => {
+    (async function () {
+      try {
+        const response = await axios.get(
+          'https://raw.githubusercontent.com/Mirumodapon/mirumo.org/link/link.json'
+        );
+        setIns(response.data);
+      } catch (error) {}
+      return () => {};
+    })();
   }, []);
 
   return (
-    <Main>
-      {sections.map((x, index) => (
-        <section key={index}>
-          <h1>{x.title}</h1>
-          {x.links.filter(linkFilter).map((y, _index) => (
-            <a key={_index} href={y.url}>
-              {y.label}
-            </a>
-          ))}
-        </section>
-      ))}
+    <Main theme={mode}>
+      <article>
+        {lns.sections.map((x) => (
+          <section key={x.title}>
+            <h1>- {x.title} -</h1>
+            {x.links.map((y) => (
+              <a href={y.url} target="_blank">
+                {y.label}
+              </a>
+            ))}
+          </section>
+        ))}
+      </article>
+      <span onClick={(e) => dispatch(changeMode())} className="themeTrigger">
+        {mode === 'dark' ? <MdLightMode /> : ''}
+        {mode === 'light' ? <MdDarkMode /> : ''}
+      </span>
     </Main>
   );
 }
